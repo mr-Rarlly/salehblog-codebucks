7597a64601c2e96031945155b2e549cdccd00ce4
mr-Rarlly <144173112+mr-Rarlly@users.noreply.github.com>
2024-01-22 13:59:55 +0200

    pixel2

--- src/utils/facebook-pixel.js
+++ src/utils/facebook-pixel.js
@@ -2,6 +2,6 @@
 import { init } from 'react-facebook-pixel';
 
 export const initFacebookPixel = () => {
-  // Replace '1811290235987717' with your actual Pixel ID
+  // Replace '1837304819762291' with your actual Pixel ID
   init('1811290235987717');
 };
