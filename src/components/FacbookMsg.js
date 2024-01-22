f4168169a308f8369997f773e31fd10e9ad6036c
mr-Rarlly <144173112+mr-Rarlly@users.noreply.github.com>
2024-01-22 19:15:19 +0200

    pixel10

--- src/components/FacbookMsg.js
+++ src/components/FacbookMsg.js
@@ -1,12 +1,13 @@
 "use client";
-import { FacebookProvider, CustomChat } from 'react-facebook';
+import { FacebookProvider, MessageUs } from 'react-facebook';
 
 export const FacbookMsg = () => {
   return (
     <FacebookProvider appId="7011338075645687" chatSupport>
-        <CustomChat pageId="101844991638707" minimized={true.toString()}/>
+        <MessageUs pageId="101844991638707" minimized={true.toString()}/>
       </FacebookProvider> 
   )
 }
 
 export default FacbookMsg;
+
