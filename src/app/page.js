37e1f66a37d31b9c8ba723bce32ae5eefdde3b1e
mr-Rarlly <144173112+mr-Rarlly@users.noreply.github.com>
2024-01-22 16:51:25 +0200

    pixel4

--- src/app/page.js
+++ src/app/page.js
@@ -3,8 +3,7 @@ import HomeCoverSection from "../components/Home/HomeCoverSection";
 import FeaturedPosts from "../components/Home/FeaturedPosts";
 import RecentPosts from "../components/Home/RecentPosts";
 import FacebookMsg from "../components/FacbookMsg";
-import Link from "next/link";
-import * as fbq from "../utils/facebook-pixel"
+
 
 export default function Home() {
   const handleClick = () => {
@@ -17,19 +16,7 @@ export default function Home() {
       <FeaturedPosts blogs={allBlogs} />
       <RecentPosts blogs={allBlogs} />
       <FacebookMsg blogs={allBlogs} />
-      <div>
-      <h1>
-        Go to `pages/_app.js` to see how you can add Facebook Pixel to your app
-      </h1>
       
-      <p>
-        Click the link below to navigate to another page.
-        <br />
-        <Link href="/navigation">Navigation page</Link>
-      </p>
-    </div>
-
-
     </main>
   )
 }
